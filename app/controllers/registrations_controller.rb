00bfb7ec0905ae98767e4e8cca3907a36180e5e4
EC2 Default User <ec2-user@ip-172-31-44-63.us-east-2.compute.internal>
2018-04-04 09:58:17 +0000

    Finish user edit action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,7 @@ class RegistrationsController < Devise::RegistrationsController
 
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        
       end
       
 end
