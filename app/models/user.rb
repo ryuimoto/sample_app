02ec112a4c40226a3e2f1febb537dc912f15d45f
EC2 Default User <ec2-user@ip-172-31-44-63.us-east-2.compute.internal>
2018-05-12 13:25:50 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,10 @@ def feed
   Micropost.where("user_id = ?", id)
 end
 
+def feed
+  Micropost.from_users_followed_by(self)
+end
+
 def following?(other_user)
   relationships.find_by(followed_id: other_user.id)
 end
